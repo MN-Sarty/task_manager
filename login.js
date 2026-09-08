3dc3fdbaaa630bae53d50103480fdd958e6fa485
226061248-ndafyaalako <226061248@nust.na>
2026-09-08 02:15:30 +0200

    Fixing TypeErrors 4

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 // Import the Supabase client
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://vtymsabwxhxalsupfday.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://vtymsabwxhxalsupfday.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0eW1zYWJ3eGh4YWxzdXBmZGF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1MDUwNzYsImV4cCI6MjEwNDA4MTA3Nn0.caTz-3TIyeeyJXwNmWsCGP0LuFtGsI5mdo9L02sW674';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
